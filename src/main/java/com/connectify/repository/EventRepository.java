0c447598bd26814980204e291aa07f2790882881
0Czar <azjounee@gmail.com>
2026-06-03 04:39:55 -0500

    Agrega consultas JPA para filtros de eventos

--- src/main/java/com/connectify/repository/EventRepository.java
+++ src/main/java/com/connectify/repository/EventRepository.java
@@ -1,7 +1,18 @@
 package com.connectify.repository;
 
 import com.connectify.entity.Event;
+import com.connectify.entity.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByStatusOrderByEventDateAsc(EventStatus status);
+
+    List<Event> findByTitleContainingIgnoreCaseAndStatusOrderByEventDateAsc(String title, EventStatus status);
+
+    List<Event> findByCityContainingIgnoreCaseAndStatusOrderByEventDateAsc(String city, EventStatus status);
+
+    List<Event> findByCategoryNameAndStatusOrderByEventDateAsc(String categoryName, EventStatus status);
 }
