edbd7676064dc450758a5965b6f70b3a9363f6c7
0Czar <azjounee@gmail.com>
2026-06-03 03:14:08 -0500

    Agrega clase principal Spring Boot

---
+++ src/main/java/com/connectify/ConnectifyApplication.java
@@ -0,0 +1,12 @@
+package com.connectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectifyApplication.class, args);
+    }
+}
