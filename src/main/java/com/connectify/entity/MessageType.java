31d7cb572fd83b9783be9b430a59e505a6f03521
0Czar <azjounee@gmail.com>
2026-06-03 06:24:04 -0500

    Agrega tipos de comunicacion interna

---
+++ src/main/java/com/connectify/entity/MessageType.java
@@ -0,0 +1,12 @@
+package com.connectify.entity;
+
+public enum MessageType {
+    EVENT_REVIEW,
+    DESIGN_REQUEST,
+    DESIGN_FEEDBACK,
+    GATE_ACCESS,
+    PAYMENT_SUPPORT,
+    TICKET_SUPPORT,
+    CUSTOMER_SUPPORT,
+    GENERAL_COORDINATION
+}
