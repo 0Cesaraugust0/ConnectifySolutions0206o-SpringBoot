ad928e5ee2386747241982ba77fc1f519063987b
0Czar <azjounee@gmail.com>
2026-06-03 06:45:49 -0500

    Agrega repositorio de mensajes internos

---
+++ src/main/java/com/connectify/repository/InternalMessageRepository.java
@@ -0,0 +1,19 @@
+package com.connectify.repository;
+
+import com.connectify.entity.InternalMessage;
+import com.connectify.entity.MessageStatus;
+import com.connectify.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InternalMessageRepository extends JpaRepository<InternalMessage, Long> {
+
+    List<InternalMessage> findByTargetRoleOrderByCreatedAtDesc(Role targetRole);
+
+    List<InternalMessage> findBySenderEmailOrderByCreatedAtDesc(String senderEmail);
+
+    List<InternalMessage> findByTargetRoleAndStatusOrderByCreatedAtDesc(Role targetRole, MessageStatus status);
+
+    long countByTargetRoleAndReadByTargetFalse(Role targetRole);
+}
