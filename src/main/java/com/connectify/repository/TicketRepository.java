2d1984be8d40adfd0f37f00a5d905ce5422f4dff
0Czar <azjounee@gmail.com>
2026-06-03 05:19:19 -0500

    Agrega repositorio Ticket

---
+++ src/main/java/com/connectify/repository/TicketRepository.java
@@ -0,0 +1,14 @@
+package com.connectify.repository;
+
+import com.connectify.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    Optional<Ticket> findByCode(String code);
+
+    List<Ticket> findByPurchaseId(Long purchaseId);
+}
