914831e7ae7dee764b26541d1ee4526c0bb815df
0Czar <azjounee@gmail.com>
2026-06-03 21:11:24 -0500

    Agrega tipos de registro administrativo de eventos

---
+++ src/main/java/com/connectify/entity/EventAdminRecordType.java
@@ -0,0 +1,10 @@
+package com.connectify.entity;
+
+public enum EventAdminRecordType {
+    CREATED,
+    UPDATED,
+    CANCELLED_BY_ORGANIZER,
+    TICKET_TYPE_CREATED,
+    TICKET_TYPE_UPDATED,
+    ADMIN_COPY
+}
